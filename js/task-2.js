342576882330d798aa8c58e803a718b0e7b62ffb
Andrii Hordiienko <30300588+drew875@users.noreply.github.com>
2025-01-28 23:07:38 +0200

    hw1

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,5 @@
+const categ = document.querySelector(".gallery");
+
 const images = [
   {
     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
@@ -22,5 +24,22 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+const markup = images.map((v) => `<img class="new-images" src="${v.url}" alt="${v.alt}">`)
+  .join("");
+
+categ.insertAdjacentHTML("beforeend", markup);
+categ.style.display = "flex";
+categ.style.flexWrap = "wrap";
+categ.style.gap = "10px";
+categ.style.height = "auto";
+categ.style.width = "100%";
+
+const addedImages = document.querySelectorAll(".new-images");
+for (let x of addedImages) {
+  x.style.width = "calc(33.333% - 10px)";
+  x.style.height = "auto";
+};
+
